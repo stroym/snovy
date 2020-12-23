0d9e50a318feeed45d5999080993aff03975073b
stroym <mikulas.papp@outlook.cz>
2020-12-24 00:27:37 +0100

    styling & split up styles into separate files

--- src/component/Search.tsx
+++ src/component/Search.tsx
@@ -1,29 +1,44 @@
 import React from "react";
 import Select from "react-select";
 
-export const menuStyle = {
-  menu: (provided: any, state: any) => ({
-    ...provided,
-    paddingTop: 0,
-    paddingBottom: 0,
-    marginTop: 0,
-    height: "100%"
-  }),
-  menuList: (provided: any, state: any) => ({
-    border: "1px solid black"
-  })
-};
+const options = [
+  {value: "text", label: "Text"},
+  {value: "title", label: "Title"},
+  {value: "tags", label: "Tags"},
+  {value: "query", label: "Query"}
+];
 
 export default class Search extends React.Component {
 
-  //TODO radio group to specify search scope
+  constructor(props: {}) {
+    super(props);
+    this.handleOptions = this.handleOptions.bind(this);
+  }
+
+  //TODO another select contains/equals etc?
+  //TODO either move tag searching into right sidebar or modify search select on tags options select
   render() {
     return (
       <span id="snovy-search">
-        <Select id="search" className="react-select" placeholder="Search by text or tags..." isSearchable
-                styles={menuStyle}/>
+        <Select id="search" className="react-select-container" classNamePrefix="react-select"
+                placeholder="Search by criteria..."
+                isClearable isSearchable
+                components={{
+                  DropdownIndicator: () => null,
+                  IndicatorSeparator: () => null,
+                  Menu: () => null
+                }}
+        />
+        <Select id="options" className="react-select-container" classNamePrefix="react-select"
+                placeholder="Select search criteria..."
+                options={options} defaultValue={options[0]}
+        />
       </span>
     );
+  }
+
+  handleOptions() {
+
   }
 
 }
--- src/component/selector/Notebook.tsx
+++ src/component/selector/Notebook.tsx
@@ -1,14 +1,13 @@
 import React from "react";
 import Creatable from "react-select/creatable";
-import {menuStyle} from "../Search";
 
 export default class NotebookSelector extends React.Component {
 
   render() {
     return (
       <span id="snovy-selector-notebook">
-        <Creatable id="notebook-select" className="react-select" placeholder="Select notebook or add new..."
-                   styles={menuStyle}/>
+        <Creatable id="notebook-select" className="react-select-container" classNamePrefix="react-select"
+                   placeholder="Select notebook or add new..."/>
       </span>
     );
   }
