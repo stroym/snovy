32b18bb7cf0c8f15c1ec12a1a6648d52ee09a737
stroym <mikulas.papp@outlook.cz>
2020-12-24 17:33:39 +0100

    search adjustment/planning

--- src/component/Search.tsx
+++ src/component/Search.tsx
@@ -4,41 +4,60 @@ import Select from "react-select";
 const options = [
   {value: "text", label: "Text"},
   {value: "title", label: "Title"},
-  {value: "tags", label: "Tags"},
-  {value: "query", label: "Query"}
+  {value: "tags", label: "Tags"}
 ];
 
-export default class Search extends React.Component {
+const values = [
+  {value: "a", label: "a"},
+  {value: "b", label: "b"},
+  {value: "c", label: "c"}
+];
+
+export default class Search extends React.Component<{}, SearchState> {
 
   constructor(props: {}) {
     super(props);
+
+    this.state = {
+      filter: options[0]
+    };
+
     this.handleOptions = this.handleOptions.bind(this);
   }
 
-  //TODO another select contains/equals etc?
-  //TODO either move tag searching into right sidebar or modify search select on tags options select
+  //TODO another select for mode selection: some (or) + all (and) - mainly tags, contains/equals (text/title)
+  // or a custom search solution, depends on how will this works with tags
+  // search scopes (section, notebook, all notebooks)?
+  // custom MenuList (show notebook, section, and matching things) unless this gets replaced by another solution
+  // OR what's most likely to happen is moving this into a sidebar where the results can be displayed in a nicer fashion
   render() {
     return (
       <span id="snovy-search">
         <Select id="search" className="react-select-container" classNamePrefix="react-select"
-                placeholder="Search by criteria..."
-                isClearable isSearchable
+                placeholder="Search by criteria..." options={values}
+                isClearable isSearchable isMulti={this.state.filter === options[2]}
                 components={{
                   DropdownIndicator: () => null,
-                  IndicatorSeparator: () => null,
-                  Menu: () => null
+                  IndicatorSeparator: () => null
                 }}
         />
         <Select id="options" className="react-select-container" classNamePrefix="react-select"
-                placeholder="Select search criteria..."
-                options={options} defaultValue={options[0]}
+                placeholder="Select search criteria..." options={options} defaultValue={options[0]}
+                onChange={this.handleOptions}
         />
       </span>
     );
   }
 
-  handleOptions() {
-
+  handleOptions(value: any): void {
+    this.setState({
+      filter: value
+    });
+    console.log(value);
   }
 
+}
+
+type SearchState = {
+  filter: { value: string, label: string }
 }
--- src/index.tsx
+++ src/index.tsx
@@ -2,9 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 
+//TODO disabled strict mode because of react-select warnings
 ReactDOM.render(
-  <React.StrictMode>
-    <App/>
-  </React.StrictMode>,
+  // <React.StrictMode>
+  <App/>,
+  // </React.StrictMode>,
   document.getElementById("root")
 );
