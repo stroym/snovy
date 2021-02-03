74b0a7fe44a9494554dfbdd2f81bd80e121c4711
stroym <mikulas.papp@outlook.cz>
2021-02-03 17:27:38 +0100

    collapsing hook

--- src/Hooks.ts
+++ src/Hooks.ts
@@ -1,9 +1,9 @@
 import React, {Dispatch, SetStateAction, useEffect, useState} from "react"
 
-export function useOutsideClick(elementRef: React.RefObject<Element | undefined>):
+export function useHideOnOutsideClick(elementRef: React.RefObject<Element | undefined>):
   [boolean, Dispatch<SetStateAction<boolean>>] {
 
-  const [inner, setInner] = useState(false)
+  const [hidden, setHidden] = useState(false)
 
   useEffect(
     () => {
@@ -17,9 +17,40 @@ export function useOutsideClick(elementRef: React.RefObject<Element | undefined>
 
   const handleOutsideClick = (e: any) => {
     if (!elementRef.current?.contains(e.target)) {
-      setInner(false)
+      setHidden(false)
     }
   }
 
-  return [inner, setInner]
+  return [hidden, setHidden]
+}
+
+export function useCollapse(elementRef: React.RefObject<HTMLElement | undefined>):
+  [boolean, Dispatch<SetStateAction<boolean>>] {
+
+  const [collapsed, setCollapsed] = useState(false)
+
+  useEffect(
+    () => {
+      // elementRef.current!.style.transition = "opacity 600"
+      elementRef.current!.classList.remove("snovy-hooks-collapsible")
+    }, []
+  )
+
+  if (elementRef.current) {
+    if (collapsed) {
+      elementRef.current.classList.remove("snovy-hooks-rolling")
+      elementRef.current.classList.add("snovy-hooks-unrolling")
+
+      // elementRef.current.style.opacity = "0"
+      // setTimeout(() => elementRef.current!.style.height = "0", 600)
+    } else {
+      elementRef.current.classList.remove("snovy-hooks-rolling")
+      elementRef.current.classList.add("snovy-hooks-unrolling")
+
+      // elementRef.current.style.height = "100%"
+      // elementRef.current.style.opacity = "1"
+    }
+  }
+
+  return [collapsed, setCollapsed]
 }
